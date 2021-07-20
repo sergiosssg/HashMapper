00f2badd66de3217d97f221277ca0639c46e273f
sergiosssg <sergiosssg@gmail.com>
2021-07-20 16:25:40 +0300

    Добавьте файлы проекта.

---
+++ DelegatesHive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashMapper
+{
+    public class DelegatesHive
+    {
+        /**
+         *    Todo 
+         * 
+         * 
+         */
+        public static int SumOfBitees(long baseOfSumm, IEnumerable<Int32> arrayOfNumbers)
+        {
+            int returnedSum = 0;
+
+            foreach(var el in arrayOfNumbers)
+            {
+                returnedSum += el;
+            }
+
+            return returnedSum;
+        }
+    }
+}

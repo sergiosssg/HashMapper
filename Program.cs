3003c5395e381ec649ecb4681a064e96fa4707c0
sergiosssg <sergiosssg@gmail.com>
2021-07-21 11:50:26 +0300

    changed Main function, for testing DelegatesHive.SumOfBitees.

--- Program.cs
+++ Program.cs
@@ -8,11 +8,12 @@ namespace HashMapper
         static void Main(string[] args)
         {
 
-            
+            int[] testArray = { 0xB4, 0xEC, 0x20, 0x52, 0x41, 0x53 };
 
 
+            long testSum = 0L;
 
-
+            testSum = DelegatesHive.SumOfBitees(testSum, testArray);
 
 
             Console.WriteLine("Всё, Хеловорд!");

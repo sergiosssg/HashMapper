f38a83070f8ce26b294315cb73e67d54daf38634
sergiosssg <sergiosssg@gmail.com>
2021-07-21 15:55:48 +0300

    added new private static function   private static byte[] StringToByteArray(string hex),     in argument, I get string, syoring array of bytes.

--- DelegatesHive.cs
+++ DelegatesHive.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HashMapper
@@ -41,5 +42,15 @@ namespace HashMapper
 
             return null;
         }
+
+
+        private static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
     }
 }
